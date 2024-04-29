0ba7d988881a824a0355218432e88e76431a3654
AntonKvitko <antonio.kvitco@gmail.com>
2024-04-29 18:21:20 +0200

    REQ-04: Show client Info (with pet)

--- src/main/java/com/magicvet/service/PetService.java
+++ src/main/java/com/magicvet/service/PetService.java
@@ -4,8 +4,13 @@ import main.java.com.magicvet.Main;
 import main.java.com.magicvet.model.Dog;
 import main.java.com.magicvet.model.Pet;
 
-public class PetService {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class PetService {
+    private static final String SIZE_PATTERN = "^(XS|S|M|L|XL|XXL)$"; // шаблон для розміру
+    private static final String SEX_PATTERN = "^(male|female)$"; // шаблон для стать тварини
+    private static final String NAME_PATTERN = "^[A-Z][a-z]*$"; // шаблон Ім'я
     private static final String DOG_TYPE = "dog";
 
     public Pet registerNewPet() {
@@ -16,10 +21,9 @@ public class PetService {
 
         if (DOG_TYPE.equalsIgnoreCase(type)) { //  якщо ТИП собака (перевіряємо без урахування регістру "IgnoreCase")
             pet = buildDog();
-        }else {
+        } else {
             pet = buildPet(type);
         }
-
         return pet;
     }
 
@@ -27,9 +31,12 @@ public class PetService {
         Pet pet = buildPet(DOG_TYPE);
         Dog dog = petToDog(pet);
 
-        System.out.print("Size (XS / S / M / L / XXL): ");
+        System.out.print("Size (XS / S / M / L / XL / XXL): ");
         dog.setSize(Main.SCANNER.nextLine().toUpperCase()); // перетворюємо в верхній регістр
-
+        while (!isSizeValid(dog.getSize())) {
+            System.out.print("Invalid input. Please enter one of the following sizes (XS, S, M, L, XL, XXL): ");
+            dog.setSize(Main.SCANNER.nextLine().toUpperCase());
+        }
         return dog;
     }
 
@@ -48,14 +55,37 @@ public class PetService {
 
         System.out.print("Sex (male / female): ");
         pet.setSex(Main.SCANNER.nextLine());
+        while (!isSexValid(pet.getSex())) {
+            System.out.print("Invalid input. Please enter one of the following sexes (male / female): ");
+            pet.setSex(Main.SCANNER.nextLine());
+        }
 
         System.out.print("Age: ");
         pet.setAge(Main.SCANNER.nextLine());
 
         System.out.print("Name: ");
         pet.setName(Main.SCANNER.nextLine());
-
+        while (!isNameValid(pet.getName())) {
+            System.out.print("Invalid input. Please enter the Name in following format (The first letter is capital, the rest are small): ");
+            pet.setName(Main.SCANNER.nextLine());
+        }
         return pet;
-
     }
+
+    private static boolean isSizeValid(String size) {
+        Pattern patternSize = Pattern.compile(SIZE_PATTERN, Pattern.CASE_INSENSITIVE); // шаблон для Size без регістра
+        Matcher matcherSize = patternSize.matcher(size);
+        return matcherSize.matches();
+    }
+    private static boolean isSexValid(String sex) {
+        Pattern patternSex = Pattern.compile(SEX_PATTERN, Pattern.CASE_INSENSITIVE); // шаблон для статі тварини без регістра
+        Matcher matcherSex = patternSex.matcher(sex);
+        return matcherSex.matches();
+    }
+    private static boolean isNameValid(String name) {
+        Pattern patternName = Pattern.compile(NAME_PATTERN); // шаблон для Ім'я
+        Matcher matcherName = patternName.matcher(name);
+        return matcherName.matches();
+    }
+
 }
